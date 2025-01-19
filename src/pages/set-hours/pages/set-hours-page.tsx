5a2202456f3c7cea9c222fac17dbca790ef39c34
Francisco Villanueva <francisco.villanueva@globant.com>
2025-01-19 14:47:45 -0300

    Feat: Created wh page editor

--- src/pages/set-hours/pages/set-hours-page.tsx
+++ src/pages/set-hours/pages/set-hours-page.tsx
@@ -1,3 +1,21 @@
+import { WorkhoursEditor } from "@/components/common/forms/wh-editor";
+import useSession from "@/hooks/useSession";
+import { IMember } from "@/interfaces/member.iterface";
+import { MemberCard } from "@/pages/members/components/member-card";
+import { useAppSelector } from "@/store/hooks";
+
 export function SetHoursPage() {
-  return <div>Set hours page</div>;
+  const { members } = useAppSelector((s) => s.member);
+  const { session } = useSession();
+
+  if (!session) return;
+  const selectedMember = members.filter((e) => e.id === session?.id)[0];
+  return (
+    <div className="space-y-4 h-full  flex flex-col">
+      <MemberCard member={session as IMember} type="read" />
+      <section className=" flex-grow">
+        <WorkhoursEditor member={selectedMember} />
+      </section>
+    </div>
+  );
 }
