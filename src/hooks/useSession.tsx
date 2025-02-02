44bbfcd7a442c6895741a1639490d524475e71a2
Francisco Villanueva <francisco.villanueva@globant.com>
2025-02-01 23:42:45 -0300

    fix build issues

--- src/hooks/useSession.tsx
+++ src/hooks/useSession.tsx
@@ -3,7 +3,6 @@ import { useAppSelector } from "@/store/hooks";
 export default function useSession() {
   const session = useAppSelector((s) => s.session);
   const { members } = useAppSelector((s) => s.member);
-  console.log(session);
   const member = members.filter((e) => e.id === session?.session?.id)[0];
-  return { session, member: member ? member : session.session };
+  return { session, member };
 }
