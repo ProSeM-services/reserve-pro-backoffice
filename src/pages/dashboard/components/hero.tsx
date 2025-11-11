09f5ebae36ee33dae3874310e37c0dbb3c8242a6
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2025-11-11 13:52:18 -0300

    fix: hide time left for free trial

--- src/pages/dashboard/components/hero.tsx
+++ src/pages/dashboard/components/hero.tsx
@@ -1,16 +1,11 @@
 import useSession from "@/hooks/useSession";
 import { useAppSelector } from "@/store/hooks";
 import { Notifications } from "./notifications-aside";
-import { checkTrialStatus } from "@/lib/utils/checkTrialStatus";
 import { CompanySwitcher } from "@/components/common/company-switcher";
 
 export default function Hero() {
   const { member } = useSession();
-  const { enterprise } = useAppSelector((s) => s.enterprise);
   const { notifications } = useAppSelector((s) => s.notifications);
-  const { currentSubscription } = useAppSelector((s) => s.subscription);
-
-  const { daysLeft, isInTrial } = checkTrialStatus(enterprise.createdAt);
 
   return (
     <header className="flex  justify-between h-full  items-center  w-full ">
@@ -19,11 +14,11 @@ export default function Hero() {
       </h2>
 
       <div className="flex items-center gap-2">
-        {isInTrial && !currentSubscription && (
+        {/* {isInTrial && !currentSubscription && (
           <div className="rounded-md p-2 bg-green-200 text-green-600">
             Quedan {daysLeft} días de prueba
           </div>
-        )}
+        )} */}
         {notifications.length ? <Notifications /> : null}
       </div>
       <div className=" bg-muted px-2 rounded-xl max-md:hidden ">
